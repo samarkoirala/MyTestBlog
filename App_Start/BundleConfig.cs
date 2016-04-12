257d23aac83745abb7278f9d1e9be0526c849718
samarkoirala <samarkoirala000@gmail.com>
2016-04-12 15:23:09 -0400

    Final

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -10,7 +10,7 @@ namespace MyBlog
     public class BundleConfig
     {
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkID=303951
-        public static void RegisterBundles(BundleCollection bundles)rthreth
+        public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/WebFormsJs").Include(
                             "~/Scripts/WebForms/WebForms.js",
